28d78ba379024cc79fbf5e954d383be97780b418
Fabrice Luraine <16@asciiland.net>
2015-11-24 15:44:07 +0100

    Add initial config for tmux

---
+++ install_or_update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# ${o:a:h} is Zsh magic for directory where script resides
+# 0 (path to script), :a (absolute path), :h (head (drop filename))
+# See http://zsh.sourceforge.net/Doc/Release/Expansion.html#Modifiers
+DOTFILES_DIR=${0:a:h}
+
+# Make a symlink in $HOME to each of the .dotfiles in stows/
+echo Installing dotfiles.
+stow --override=".*" --restow --target=$HOME --dir="$DOTFILES_DIR" stows
+
+if [ -d "$DOTFILES_DIR/local_$(hostname -s)" ]; then
+	echo "Using local overrides ($DOTFILES_DIR/local_$(hostname -s))"
+	stow --override=".*" --restow --target=$HOME --dir="$DOTFILES_DIR" "local_$(hostname -s)"
+else
+	echo "No local overrides found ($DOTFILES_DIR/local_$(hostname -s))"
+fi
